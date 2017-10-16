d4686885d7c2c25a209ea6e52f7f5843eb8608bc
Martin Cervenka <cervemar@students.zcu.cz>
2017-10-16 11:21:41 +0200

    Smazan chraneny rezim

--- main.asm
+++ main.asm
@@ -33,26 +33,11 @@ dw 0xaa55                         ; vlozeni bajtu 0xAA a 0x55 -> jedna se o boot
 dalsi_sektory:
 	call obrazek_zobrazit     ; zobrazeni uvodniho obrazku
 	jmp $
-	;mov eax, zprava_boot     ; vyber zpravy k vypisu
-	;call pis16               ; vypis zpravy v realnem modu
-	;call prepnuti_chraneny   ; prepnuti procesoru do chraneneho rezimu (dojde ke skoku na navesti "chraneny")
-	;jmp $                    ; nekonecna smycka (skok na sebe sama)
-
 
 %include "splash.asm"             ; vlozeni nacitaci obrazovky
 %include "gdt.asm"                ; vlozeni definice global descriptor table
-%include "protected.asm"          ; vlozeni kodu pro prechod do chraneneho (protected) modu procesoru
 %include "print.asm"              ; vlozeni funkci pro vypis v realnem a chranenem modu
 
-bits 32                           ; zacatek kodu v chranenem 32bitovem rezimu
-chraneny:
-	mov eax, zprava_chraneny  ; vyber zpravy k vypisu
-	xor ebx, ebx              ; vynulovani EBX - zprava bude psana na zacatek obrazovky
-	mov ch, 0x0f              ; vyber fontu
-	call pis32                ; vypsani zpravy v chranenem modu
-	jmp $                     ; nekonecna smycka (skok na sebe sama)
-zprava_chraneny:
-	db "Prepnuto do chraneneho modu!", 0xa, "Vsechno OK", 0
 druha_radka:
 	db "Dalsi radka!", 0
 zprava_boot:
