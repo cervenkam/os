4e9f9514d6657779dc4b6783b9096dc6ba414c85
vrbik <tondavrba@gmail.com>
2017-10-30 23:47:52 +0100

    vterinovy citac, ktery odmita pracovat

--- browser/browser.asm
+++ browser/browser.asm
@@ -1,8 +1,20 @@
 org 0
 bits 16
-interrupt_handler:
-call pis16_registry
-iret
+interrupt_handler: ;nefunguje :'(
+	push ax
+	xor ax, ax
+	mov ax, [odpocet_jak_krava]
+	cmp ax, 0
+	je cas_vyprsel
+	dec ax
+	mov [odpocet_jak_krava], al ;slo by asi dec byte [odpocet_jako_krava]
+	pop ax
+	iret
+cas_vyprsel:
+	pop ax
+	call pis16_registry
+	mov byte [odpocet_jak_krava], 18
+	iret
 
 start:
 	mov ax, cs                ; zkopirovani code segmentu do AX
@@ -12,6 +24,8 @@ start:
 	mov bp, 0x9000            ; nastaveni bazove adresy zasobniku
 	mov sp, bp                ; a ukazatele na aktualni prvek zasobniku (stack pointeru)
 	jmp $
+
+odpocet_jak_krava db 18
 
 %include "print.asm"
 ;zacne hazet chybu pri rostoucim kodu, proto pak zvysit ale
